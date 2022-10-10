ec276a7ab84d607940b4e0b720a81dad7000cbb5
diveshthakre19 <diveshthakre07@.com>
2022-10-10 08:48:40 +0000

    first commit

---
+++ 01Accordion/faq.js
@@ -0,0 +1,36 @@
+const faqData = [
+  {
+    id: 1,
+    question: "Who are we?",
+    answer:
+      "We enable upscaling careers through flexible, interactive and collaborative learning. We believe in building learning communities by bringing together mentors, young minds, and creators.",
+  },
+  {
+    id: 2,
+    question: "What do we do?",
+    answer:
+      "Building learning communities with Our Affordable & Competent Courses",
+  },
+  {
+    id: 3,
+    question: "Are the community classes boring?",
+    answer: "No, not at all!!!",
+  },
+];
+
+const accordianBody;
+const faqs = [];
+
+function showFaq() {
+  
+}
+
+function createFaq() {
+  
+}
+
+function btnStatusUpdate() {
+  
+}
+
+
