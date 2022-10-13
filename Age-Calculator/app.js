5edc005059faa9e02fca7bbec0001000334782fe
diveshthakre19 <diveshthakre07@.com>
2022-10-13 05:07:51 +0000

    age calc cpmplete

--- Age-Calculator/app.js
+++ Age-Calculator/app.js
@@ -1,17 +1,25 @@
-const container;
-const error;
+const container = document.querySelector(".container");
+const display = document.querySelector(".desc");
+const error = document.querySelector(".error");
+const inputAge = document.querySelector("#dob");
+const newDate = new Date();
 
-function getAge(){
-    
-    
+function getAge() {
+  age = inputAge.value;
+  let bithyear = age.slice(0, 4);
+  let birthmonth = age.slice(5, 7);
+  let birthdate = age.slice(8, 10);
+  let currentYear = newDate.getFullYear();
+  let currentMonth = newDate.getMonth();
+  let currentDate = newDate.getDate();
+  let userAge = currentYear - Number(bithyear);
+  let usermonth = currentMonth - Number(birthmonth);
+  let userdays = Number(birthdate) - currentDate;
+  display.innerHTML = `Your Age is : ${userAge} Years, ${usermonth} months & ${userdays} Days`;
+  if (!age) {
+    display.innerHTML = "Please enter your Birthdate";
+  }
+  if (bithyear > 2022) {
+    error.style.display = "block";
+  }
 }
-
-function calcAge(){ 
-    
-}
-
-function displayAge(){
-    
-    
-}
-
